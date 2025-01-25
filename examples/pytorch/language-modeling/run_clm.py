fe9044ec4a667ec25c3591e1cc133c68f57163fa
jeongjaeyong <42512062+jeongjaeyong@users.noreply.github.com>
2025-01-25 20:09:13 +0900

    Update run_clm.py

--- examples/pytorch/language-modeling/run_clm.py
+++ examples/pytorch/language-modeling/run_clm.py
@@ -228,13 +228,6 @@ class ModelArguments:
         default=False,
         metadata={"help": ("Enable PyTorch/XLA Pallas Flash Attention")},
     )
-    def __post_init__(self):
-        if self.config_overrides is not None and (
-            self.config_name is not None or self.model_name_or_path is not None
-        ):
-            raise ValueError(
-                "--config_overrides can't be used in combination with --config_name or --model_name_or_path"
-            )
 
 
 @dataclass
@@ -312,36 +305,6 @@ class DataTrainingArguments:
         default=True,
         metadata={"help": "Whether to keep line breaks when using TXT files or not."},
     )
-
-    def __post_init__(self):
-        if self.streaming:
-            require_version(
-                "datasets>=2.0.0", "The streaming feature requires `datasets>=2.0.0`"
-            )
-
-        if (
-            self.dataset_name is None
-            and self.train_file is None
-            and self.validation_file is None
-        ):
-            raise ValueError(
-                "Need either a dataset name or a training/validation file."
-            )
-        else:
-            if self.train_file is not None:
-                extension = self.train_file.split(".")[-1]
-                assert extension in [
-                    "csv",
-                    "json",
-                    "txt",
-                ], "`train_file` should be a csv, a json or a txt file."
-            if self.validation_file is not None:
-                extension = self.validation_file.split(".")[-1]
-                assert extension in [
-                    "csv",
-                    "json",
-                    "txt",
-                ], "`validation_file` should be a csv, a json or a txt file."
 
 
 def main():
