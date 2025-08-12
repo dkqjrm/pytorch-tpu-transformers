6e6fd2c9f80576369c2c25aa8b57a70cff0bd347
dkqjrm <dkqjrm@naver.com>
2025-08-12 15:04:48 +0900

    test

--- run_id_3b.sh
+++ run_id_3b.sh
@@ -12,12 +12,12 @@ export PROFILE_LOGDIR=~/log
 python examples/pytorch/language-modeling/run_clm.py \
   --model_name_or_path /home/hyun/Qwen3-0.6B \
   --tokenizer_name Qwen/Qwen2-0.5B \
-  --dataset_name dkqjrm/korean-english-qwen-templated \
+  --dataset_name dkqjrm/korean-english-qwen-templated-small \
   --per_device_train_batch_size 4 \
   --per_device_eval_batch_size 4 \
-  --gradient_accumulation_steps 32 \
+  --gradient_accumulation_steps 16 \
   --num_train_epochs 1 \
-  --save_steps 1000 \
+  --save_steps 10 \
   --logging_steps 1 \
   --do_train \
   --do_eval \
