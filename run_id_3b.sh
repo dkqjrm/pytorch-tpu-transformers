ef5a667f49627d077da27a7d1a4fd4cda6297d3e
dkqjrm <dkqjrm@naver.com>
2025-08-12 18:04:04 +0900

    test

--- run_id_3b.sh
+++ run_id_3b.sh
@@ -15,7 +15,7 @@ python examples/pytorch/language-modeling/run_clm.py \
   --dataset_name dkqjrm/korean-english-qwen-templated \
   --per_device_train_batch_size 2 \
   --per_device_eval_batch_size 2 \
-  --gradient_accumulation_steps 4 \
+  --gradient_accumulation_steps 8 \
   --num_train_epochs 1 \
   --save_steps 200 \
   --logging_steps 1 \
